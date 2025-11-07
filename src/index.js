12c080eb9b9fbfc35d89cdd38b1644fbe3966ad0
danielcruz604 <dancruzsan6@gmail.com>
2025-11-07 11:29:25 -0300

    feat: adiciona conexão com o banco de dados

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
+const conexao = require('./models/db');
+
+
+conexao.connect((erro) => {
+    if (erro) {
+        console.error('Erro ao se conectar no DB')
+        return;
+    }
+    app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+    console.log('Conectado ao MySQL com sucesso!')
+})

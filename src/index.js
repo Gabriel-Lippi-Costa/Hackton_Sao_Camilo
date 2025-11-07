92f0bdc9d64aa8b8827ff158d75b8df2bc058a24
Arthur Monteiro <arthurmonteiro.amb@gmail.com>
2025-11-07 15:24:08 -0300

    feat: adiciona as rotas de dashboard

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ const path = require('path');
 const app = express();
 const conexao = require('./models/db');
 const rotasLogin = require('./routes/authenticationRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
 
 // === Middlewares ===
 app.use(cors());
 app.use(express.json());
 app.use('/auth', rotasLogin); // ✅ todas as rotas começam com /auth
 
+app.use('/api', dashboardRoutes);
 // === Conexão ao banco e start do servidor ===
 conexao.connect((erro) => {
   if (erro) {
